e470adf0582c89bc5a75cbbe6f4d7aba1955946e
Piotr Kaczynski <s33387@pjwstk.edu.pl>
2026-03-10 11:16:59 +0100

    Line correction

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -8,9 +8,9 @@ static double CalculateAverage(int[] values)
 {
     int sum = 0;
     foreach (int value in values)
-        {
+    {
         sum += value;
-        }
+    }
     return sum / values.Length;
 }
 
